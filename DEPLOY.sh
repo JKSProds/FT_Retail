1e5e944f5e7711902ed063f03583a2713315347f
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-10 23:12:43 +0000

    Adicionado script para deploy automático!

---
+++ DEPLOY.sh
@@ -0,0 +1,22 @@
+cd ~/FT_Retail
+
+git clone https://github.com/JKSProds/FT_Retail.git
+
+cd FT_Retail/FT_Retail
+
+docker build -t aspnetapp .
+
+docker stop FT_Retail
+docker rm FT_Retail
+
+cd ..
+cd ..
+
+chmod -R 777 FT_Retail
+
+rm -r FT_Retail
+
+docker run -d -p 8080:80 --name FT_Retail aspnetapp
+
+
+
